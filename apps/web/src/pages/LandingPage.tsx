4591b36e22c52185e20e98d709091f5e1a2fe251
Vebjørn Baustad <vBaustad@users.noreply.github.com>
2026-01-16 11:35:51 +0100

    wip

--- apps/web/src/pages/LandingPage.tsx
+++ apps/web/src/pages/LandingPage.tsx
@@ -1,26 +1,16 @@
-/**
- * Fil: LandingPage.tsx
- * Utvikler(e): Vebjørn Baustad, Ramona Cretulescu, Synne Oppberget
- * Beskrivelse: Selve landingssiden som inneholder flere komponenter som bygger opp hele siden. LandingHero, Strukturert landingside for videre utvidelse med egne seksjoner, flere landing-komponenter ( hero, features. )  
- */
-
-
-import { useState } from "react";
-import LandingHero from "../components/LandingHero";
-import LandingFeatures from "../components/landing/LandingFeatures";
-import LandingActivities from "../components/landing/LandingActivities";
-
+import LandingHero from "../components/landing/LandingHero";
+import LandingSidebarSection from "../components/landing/LandingSidebarSection";
+import PartnersSection from "../components/sections/PartnerSection";
 
 export default function LandingPage() {
-  const [openRule, setOpenRule] = useState<number | null>(null);
-
-  
   return (
     <>
       <LandingHero />
       <main className="mx-auto max-w-5xl px-4 py-10">
-        {/* resten av forsiden */}
+        <LandingSidebarSection />
       </main>
+
+      <PartnersSection />
     </>
   );
 } 
